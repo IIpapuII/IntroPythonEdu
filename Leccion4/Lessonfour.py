1e20d646220e74e3580d63512b660f7e7e1a25b4
IIpapuII <wnserrano@misena.edu.co>
2023-01-25 23:29:39 -0500

    Ingreso de nuevo ejercicio para desarrollar

--- Leccion4/Lessonfour.py
+++ Leccion4/Lessonfour.py
@@ -8,9 +8,14 @@ def NumberArrayOne(number):
             print(f'El siguiente valor  no termina en uno: {i}')
    
 """ 
-3. Mostrar 10 veces la palabra Programacion
+31. Mostrar 10 veces la palabra Programacion
 """
 def PrinterString():
     i =0
     while i<= 10:
         print("Programacion")
+
+'''32. Del conjunto de cadenas ["E","F","M","A"], que corresponden 
+a las iniciales  de los primeros cuatro meses del año, mostrar el nombre  completo
+de dichos meses usando sentencia repetitivas y condicionales.
+'''
