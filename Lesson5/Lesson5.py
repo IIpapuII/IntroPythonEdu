a56ef6c0d3c9b1557c7c9a4a82263ced55006837
IIpapuII <wnserrano@misena.edu.co>
2023-02-04 23:48:28 -0500

    Plantiamiento de los ejercicios de las leccion 5 junton con las funciones que se van a trabajar

---
+++ Lesson5/Lesson5.py
@@ -0,0 +1,72 @@
+#Desarrollo de la quinta lección sobre funciones los ejercicios a desarrollar
+
+
+""" 
+45. Construir una funccion que reciba su nombre como parametro y que lo muestre 5 veces
+
+"""
+def print_name():
+    pass
+
+'''
+46. Construir una funcion que reciba una cadena digitada (como parametro)
+por el usuario y que lo muestre n veces en pantalla, El valor de n tambien digitado por el usuario
+'''
+def repeat_text():
+    pass
+
+'''
+47. Construir  una funcion que reciba (como parametro) una lista de datos numericos y retorne
+la suma de dichos numeros.
+'''
+def sum_of_number():
+    pass
+
+'''
+48. Constuir una funcion que reciba (como parametro) una lista de datos numericos
+y retorne el promedio de los datos pares.
+'''
+def avarage_pairs():
+    pass
+
+'''
+49. Construir una funcion que reciba (como parametro) una lista de datos numericos
+y retorne el promedio de dichos datos.
+'''
+def avarage_data():
+    pass
+
+'''
+50. Construir una funcion que reciba (como parametros) una lista de cadenas
+y retorne la suma de la cantidad de letras de cada cadena
+'''
+def add_string():
+    pass
+
+'''
+51. Construir una funcion que reciba dos datos enteros (como parametros ) 
+y retorne el dato mayor
+'''
+def greater_data():
+    pass
+
+'''
+52. Construir una funcion que reciba un valor entero (como parametro)
+ y muestre la tabla de multiplicar de dicho valor recibido.
+'''
+def multiplication_table():
+    pass
+
+'''
+53. Construir una funcion que reciba un valor entero (como parametro) y determine si su ultimo digito es 4
+
+'''
+def last_digit_four():
+    pass
+
+'''
+54. Construir una funcion que reciba una lista de datos enteros (como parametro) y
+retorne la cantidad de valores de la lista que terminan en 4.
+'''
+def last_digit_list():
+    pass
