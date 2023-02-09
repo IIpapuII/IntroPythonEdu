b3c28b6b4ee26be5f2bdbf747a900d86782d32a2
IIpapuII <wnserrano@misena.edu.co>
2023-02-08 22:26:43 -0500

    Se agregan los nuevos ejercicios de la leccion 6

---
+++ Lesson6/Lesson6.py
@@ -0,0 +1,36 @@
+# Desrrollo de la leccion 6 generadores
+
+'''
+55. Construir un programa que muestre los numeros multiplos de 4 comprendidos en el rango de 1 y un tope dado.
+
+'''
+def number_multiples():
+    pass
+
+'''
+56. Constuir un programa que  muestre los numeros terminados en 3 comprendidos entre 1  y un
+tope dado.
+'''
+def number_printer_stop():
+    pass
+
+'''
+57. Construir un programa que muestre los numeros cuyos  dos ultimos digitos sumen 4,
+en el rango comprendidos entre 1 un numero dado
+'''
+def sum_four_limit():
+    pass
+
+'''
+58. Constuir un programa que muestre los numeros cuyos dos ultimos digitos sean iguales,
+en el rango comprendido entre 1 y un numero dado.
+'''
+def equal_number_end():
+    pass
+
+'''
+59. Constuir un programa que muestre los numeros cuyos 3 ultimos digitos sumen 8
+en el rago comprendido entre 1 y un numero dado.
+'''
+def sum_eight_limit():
+    pass
