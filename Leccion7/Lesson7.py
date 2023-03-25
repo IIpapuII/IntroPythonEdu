8e7933d8ecb2541a23455c747693b349db2f1284
IIpapuII <wnserrano@misena.edu.co>
2023-03-24 23:16:04 -0500

    Se añade estructura de los ejercicios 7

---
+++ Leccion7/Lesson7.py
@@ -0,0 +1,24 @@
+""" 
+63. Construir un programa que permita leer un numero y calcular la suma de 
+sus3 ultimos digitos
+"""
+
+""" 
+64. Construir un programa que permita leer un numero y mostrar su raiz cuadrada
+(para calcular la raiz se calcula  usando math.sqrt) y acontinuacion el valor al cual
+se le quiere calcular
+"""
+
+""" 
+65. Construir un programa que permita  leer dos numeros enteros  y calcular  el resultado de elevar
+el 1er numero a la potencia representada por el 2 numero
+
+"""
+
+""" 
+66. Construir un programa que permita  leer una cadena y mostrar cuantos caracteres tiene.
+
+"""
+""" 
+67. Construir un programa que permita leer una cadena y  mostrar cuantas palasbras tiene.
+"""
