a9a1998423f33f1b176a8afdb9e4bd72546bbd3d
IIpapuII <wnserrano@misena.edu.co>
2023-03-24 23:42:30 -0500

    Se relaciona toda la estructura de ejercicios a desarrollar

--- Leccion7/Lesson7.py
+++ Leccion7/Lesson7.py
@@ -1,6 +1,6 @@
 """ 
 63. Construir un programa que permita leer un numero y calcular la suma de 
-sus3 ultimos digitos
+sus 3 ultimos digitos
 """
 
 """ 
@@ -21,4 +21,22 @@ el 1er numero a la potencia representada por el 2 numero
 """
 """ 
 67. Construir un programa que permita leer una cadena y  mostrar cuantas palasbras tiene.
+"""
+
+"""
+68. Construir un programa que permita leer  una cadena y mostrar cuantas vocales tiene
+"""
+
+""" 
+69. Construir un programa que permita leer una cadena y mostrar cuantas veces contiene la letra A (mayuscula o minuscula)
+"""
+""" 
+70. Construir un programa que permita leer una cadena y mostrar cuantas veces contiene una letra digitada por el usuario
+"""
+""" 
+71. Construir un programa que permita leer un numero y mostrar el resultado  de dividirlo entre 4.
+
+"""
+""" 
+72.  Constuir un programa que permita leer dos numeros enteros y determinar si tienen la misma cantidad de digitos. 
 """
