bfdb3f1790017e8ffb1a0433b7cda3c0435dd15b
wilmer serrano <papu@iMac-de-wilmer.bbrouter>
2023-04-27 00:07:23 -0500

    Definicion de las clases de los ejercicios propuestos.

---
+++ Leccion8/Lesson8.py
@@ -0,0 +1,62 @@
+"""Conceptos basicos de POO Ejercicios de instanciacion """
+
+"""
+74. Construir un programa python que declare una clase de tipo ANIMAL y 
+que instancie un objeto llamado INSECTO.
+"""
+class Animal:
+    pass
+"""
+75. Construir un programa python que declare una clase VEHICULO y que instancie
+un objeto llamado AVION.
+"""
+class Vehicle:
+    pass
+"""
+76. Construir un programa python de declares una clase VEHICULO y que instancie 
+un objeto llamado CARRO
+
+"""
+
+"""
+77. Construir un programa python que declare una clase COMIDA y que instancie
+ub objeto llamado ARROZCONPOLLO.
+"""
+class Food:
+    pass
+"""
+78. Construir un programa python que declare una clase ARBOL y que instancie un 
+objeto llamado CEDRO.
+"""
+class Tree:
+    pass
+"""
+79. Construir un programa python que declare una clase LIBRO  y que intancie un 
+objeto llamado NOVELA.
+"""
+class Book:
+    pass
+"""
+80. Construir un programa python que declare una clase  PELICULA  y que instancie un
+objeto llamado  MONSTERSINC.
+"""
+class Movie:
+    pass
+"""
+81. Constuir un programa python que declare una clase DISPOSITIVO  y que instancie un
+objeto llamado PORTATIL.
+"""
+class Device:
+    pass
+"""
+82. Construir un programa python que declare una clase SERVICIO y que instancie un 
+objeto llamado ENERGIA.
+"""
+class Service:
+    pass
+"""
+83. Construir un programa python que declare una clase EMPAQUE  y que instancie un
+objeto llamado CAJA.
+"""
+class Paking: 
+    pass
