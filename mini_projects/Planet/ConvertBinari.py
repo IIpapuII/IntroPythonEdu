970db569ceeefc4f5354ff55cb0132d3e05f828c
IIpapuII <wnserrano@misena.edu.co>
2023-07-20 10:15:10 -0500

    Ultimos cambios al proceso de la estrucutra de la lección 7

---
+++ mini_projects/Planet/ConvertBinari.py
@@ -0,0 +1,12 @@
+
+def converDecimalBinari():
+    '''
+    Función encargada de convertir de decimal a binario mediante list comprehension
+    '''
+    numberDecimal= int(input('Ingresar un numero decimal: '))
+    resultBinari = []
+    print(resultBinari)
+
+
+if __name__ == '__main__':
+    converDecimalBinari()
