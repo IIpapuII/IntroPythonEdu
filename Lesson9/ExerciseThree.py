16bc7b2ae28138b7761cf2577c45ee14ccf8108e
IIpapuII <wnserrano@misena.edu.co>
2023-08-23 10:00:41 -0500

    Se instanacia la clase para resolver el ejercicio 3

--- Lesson9/ExerciseThree.py
+++ Lesson9/ExerciseThree.py
@@ -6,3 +6,18 @@ le permita almacenar las 10 principales características  de los autos.
 el precio  de venta de cada auto es igual al precio de compra multiplicado
 por 1.4 que corresponde a su margen de ganancia. 
 """
+
+class vehicles:
+    def __init__(self, sourceType:str, purchasePrice:int, vehiclePlaca:str, color:str, engine:int,power:int,torque:int, typeOfBox:str, ):
+        self._wheels = 4 
+        self._stalls = 5
+        self._profitability = 1.4
+        self.sourceType = sourceType
+        self.salePrice = 0
+        self.purchasePrice = purchasePrice
+        self.vehiclePlate = vehiclePlaca
+        self.color = color
+        self.engine= engine
+        self.power = power
+        self.torque = torque
+        self.typeOfBox = typeOfBox
