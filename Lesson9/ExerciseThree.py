4831d8754124aeb7d948e21fb057ffdc39338e80
IIpapuII <wnserrano@misena.edu.co>
2023-08-24 10:06:41 -0500

    Function is added for data entry and profitability calculation

--- Lesson9/ExerciseThree.py
+++ Lesson9/ExerciseThree.py
@@ -21,3 +21,36 @@ class vehicles:
         self.power = power
         self.torque = torque
         self.typeOfBox = typeOfBox
+
+
+    def calculPriceVehicle(self):
+        self.salePrice = self.purchasePrice * self._profitability
+    
+    def EnterData():
+        choiseSourse =  int(input("Ingrese el valor según el tipo de vehiculo a registrar: 1- Nacionales 2- Importados: " ))
+        if choiseSourse == 1:
+            sourseType = 'Nacionales'
+        elif choiseSourse  == 2:
+            sourseType = 'Importados'
+        else:
+            print('Incorrect selection')
+        
+        purchasePrice = int(input("Ingrese el precio del compra del  vehiculo: "))
+        vehiclePlaca = input("Ingrese la placa de registro del vehiculo: ")
+        color = input("Ingrese el color del vehiculo: ")
+        engine = int(input("Ingrese el tipo de motor del vehiculo: "))
+        power = int(input("Ingrese la potencia del motor: "))
+        torque= int(input("Ingrese el torque que maneja el vehiculo: "))
+
+        choiseTypeBox = int(input("Ingrese el tipo de caja que maneja el Vehiculo: 1- Automatica 2-Manual: "))
+        if choiseTypeBox == 1:
+            typeOfBox = 'Automatica'
+        elif choiseTypeBox == 2:
+            typeOfBox = 'Manual'
+        else:
+            print('Incorrect selection')
+
+        return sourseType, purchasePrice, vehiclePlaca, color, engine, power, torque, typeOfBox
+
+def mainVahicles():
+    pass
