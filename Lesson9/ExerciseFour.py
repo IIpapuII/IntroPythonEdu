ca15a33328a995a5a4777bed8b21a57a3da8d18f
IIpapuII <wnserrano@misena.edu.co>
2023-08-28 10:47:20 -0500

    class from exercise four is added

--- Lesson9/ExerciseFour.py
+++ Lesson9/ExerciseFour.py
@@ -5,3 +5,31 @@ Se requiere almacenar sus 6 principales características. Todos son
 productos importados  y su precio de venta es igual a su precio de compra 
 multiplicado por 1.7 que corresponde  a su margen de ganancia.  
 """
+class  Devices:
+    def __init__(self, typeOfDevices:str,ram:int, storage:int, purchasePrice:int):
+        self.typeOfDevices = typeOfDevices
+        self.brand = 'PHR'
+        self.ram = ram
+        self.storage = storage
+        self.salesPrice = 0
+        self.PurchasePrice = purchasePrice
+        self.profitability = 1.7
+    
+    def calcSalesPrice(self):
+        self.salesPrice = self.PurchasePrice * self.profitability
+    
+    def inputDataDevices():
+        choiseTypeDevices = int(input("Ingrese la selección del tipo de dispositivo : 1- Celulares 2- Tablets 3- Portátiles:"))
+        if choiseTypeDevices == 1:
+            typeOfDevices = 'Celulares'
+        elif choiseTypeDevices == 2:
+            typeOfDevices = 'Tablets'
+        elif choiseTypeDevices  == 3:
+            typeOfDevices = 'Portatiles'
+        else:
+            print("Selección incorrecta")
+        ram = int(input("Ingrese la cantidad de RAM del dispositivo: "))
+        storage = int(input("Ingrese la cantidad de almacenamiento del dispositivo: "))
+        purchasePrice = int(input("Ingrese el precio de compra: "))
+        return typeOfDevices, ram, storage, purchasePrice
+
