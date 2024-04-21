44c2a9cd37f2b90a170a801d566fd3b1f961ceef
IIpapuII <wnserranoa@unadvirtual.edu.co>
2024-04-21 15:10:12 -0500

    Avances en el desarrollo de los ejecicios del libro

---
+++ Lesson12/Exercice1.py
@@ -0,0 +1,61 @@
+
+a = input("Ingrese una cadena: ")
+b = input("Ingrese una subcadena: ")
+def valide_initial_sudstring(a,b):
+    """ 
+1.Construir un programa que lea una cadena y una subcadena y determine si la cadena
+empieza con los caracteres de la subcadena
+"""
+    if a.startswith(b) == True:
+        print( f'PResenta una sudcadena de {b}')
+    else:
+        print("No inicia con la subcadena")
+        
+
+
+def valide_end_sudstring(a,b):
+    """ 
+2. Construir un programa que lea una cadena y nuna subcadena y determine si la cadena
+finaliza  con los caracteres de la subcadena
+"""
+    if a.endswith(b):
+        print(f'Presenta {b} la sudcadena final')
+    else:
+        print(f'No se presenta una sudcadena')
+
+#valide_end_sudstring(a,b)
+
+def replace_substring(a,b):
+    """ 
+3. Construir un programa que lea  una cadena  y, dentro de ella, reemplace
+una subcadena por otra (Tambien leidas)
+"""
+    if b in a :
+        c = input("ingrese la cadena a remplazar: ")
+        d = a.replace(b,c)
+        print(f'Nueva cadena : {d}')
+    else:
+        print(f'Sudcadena no puede ser remplazada no existe {b}')
+
+print(replace_substring(a,b))
+""" 
+4. Construir un programa que lea auna cadena que contiene mayúsculas y  minúsculas  y que
+muestre una cadena con las mayúsculas convertidas en minusculas y las minúculas convertidas en
+mayúsculas .
+"""
+
+""" 
+5. Construir un programa que lea una cadena  y convierta su contenido en minúsculas.
+"""
+
+""" 
+6. Construir un programa que lea una cadena  y convierta su contenido en mayúsculas.
+"""
+
+""" 
+7. Construir  un programa que lea una cadena  y la muestre con su 1er carácter  convertido
+a mayúscula  y el resto a minúscula.
+"""
+
+
+
